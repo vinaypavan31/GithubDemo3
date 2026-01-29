f867681b796bcdcbf00ffadb9dc3c5b4aa13425b
ramsai <ramsai14264@gmail.com>
2026-01-29 11:27:33 +0530

    adde comment to test conflict

--- GithubDemo3/src/main/java/com/pages/LoginPage.java
+++ GithubDemo3/src/main/java/com/pages/LoginPage.java
@@ -5,5 +5,6 @@ public class LoginPage {
 	public static void main(String[] args) {
 		System.out.println("this is login page");
 		System.out.println("this is made by tester ");
+		System.out.println("added new li eto test the conflict");
 	}
 }
