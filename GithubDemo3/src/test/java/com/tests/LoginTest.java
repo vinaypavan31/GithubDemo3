f2d8d345bb2cf4f22d5d3adcd644aa941091dd63
vinaypavan31 <vinaypavan14264@gmail.com>
2026-01-29 10:19:59 +0530

    this is the primary framework from QA lead

---
+++ GithubDemo3/src/test/java/com/tests/LoginTest.java
@@ -0,0 +1,7 @@
+package com.tests;
+
+public class LoginTest {
+	public static void main(String[] args) {
+		System.out.println("this is test login");
+	}
+}
---
+++ GithubDemo3/src/test/resources/TestDataProvider.java
@@ -0,0 +1,9 @@
+
+public class TestDataProvider {
+
+	public static void main(String[] args) {
+		System.out.println("this is test data provider");
+
+	}
+
+}
